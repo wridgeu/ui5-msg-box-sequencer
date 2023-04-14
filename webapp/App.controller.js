1116508d2577b627e46b40db519474361bdb4e6f
Marco Beier <marco-beier@t-online.de>
2023-04-15 01:20:36 +0200

    chore: some cleanup

--- webapp/App.controller.js
+++ webapp/App.controller.js
@@ -1,21 +1,16 @@
 sap.ui.define(
   [
     "sap/ui/core/mvc/Controller",
-    "sap/m/InstanceManager",
-    "sap/m/MessageBox",
-    "sap/ui/base/EventProvider",
     "Demo/MessageBoxSequencer"
   ],
-  function (Controller, InstanceManger, MessageBox, EventProvider, MessageBoxSequencer) {
+  function (Controller, MessageBoxSequencer) {
     "use strict";
     return Controller.extend("Demo.App", {
       onInit: function () {
         this._messageBoxSequencer = new MessageBoxSequencer()
-        // this._messageToBeShownLater = []
       },
 
-      onButton: function () {
-        // pretend a WebSocket message and some further messages after immediately afterwards
+      onButton() {
         let value;
         for (let i = 0; i < 5; ++i) {
           value = this.byId(`input${i}`).getValue()
@@ -26,39 +21,9 @@ sap.ui.define(
         }
       },
 
-      _webSocketHandler: function (text) {
+      _webSocketHandler(text) {
         this._messageBoxSequencer.handleMessage(text)
       }
-      // _webSocketHandler: async function (text) {
-      //   let skip = await new Promise((res) => {
-      //     InstanceManger.getOpenDialogs().forEach((dialog) => {
-      //       if (dialog.getId() === "test") {
-      //         this._messageToBeShownLater.push(text)
-      //         // register callback to currently opened popup
-      //         if (!EventProvider.hasListener(dialog, "afterClose", this._afterCloseHandler, this) && this._messageToBeShownLater.length !== 0) {
-      //           dialog.attachAfterClose(this._afterCloseHandler, this)
-      //         }
-      //         res(true);
-      //       }
-      //     })
-      //     res(false);
-      //   })
-      //   if (skip) {
-      //     return;
-      //   }
-      //   await new Promise((res) => {
-      //     MessageBox.show(text.toString(), {
-      //       icon: MessageBox.Icon.INFORMATION,
-      //       id: "test",
-      //       actions: [MessageBox.Action.YES, MessageBox.Action.NO],
-      //       onClose: (oAction) => { res("") },
-      //     });
-      //   })
-      // },
-
-      // _afterCloseHandler: async function () {
-      //   await this._webSocketHandler(this._messageToBeShownLater.pop())
-      // }
     });
   }
 );
